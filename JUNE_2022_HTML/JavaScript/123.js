ad77bb8fb395bae87ffa22c8ee92369bf931670a
1oleg5 <panolegv@gmail.com>
2022-08-12 22:51:20 +0300

    H/W_4d

---
+++ JUNE_2022_HTML/JavaScript/123.js
@@ -0,0 +1,64 @@
+let hello = 'hello';
+console.log(hello);
+document.write(`${hello} `);
+alert(hello);
+
+let owu = 'owu';
+console.log(owu);
+document.write(`${owu}.`);
+alert(owu);
+
+let com = 'com';
+console.log(com);
+document.write(`${com}.`);
+alert(com);
+
+let ua = 'ua';
+console.log(ua);
+document.write(`${ua} `);
+alert(ua);
+
+let q = '1';
+console.log(q);
+document.write(`${q} `);
+alert(q)
+
+let w = '10';
+console.log(w);
+document.write(`${w} `);
+alert(w);
+
+let e = '-999';
+console.log(e);
+document.write(`${e} `);
+alert(e);
+
+let r = '123';
+console.log(r);
+document.write(`${r} `);
+alert(r);
+
+let t = '3.14';
+console.log(t);
+document.write(`${t} `);
+alert(t);
+
+let y = '2.7';
+console.log(y);
+document.write(`${y} `);
+alert(y);
+
+let u = '16';
+console.log(u);
+document.write(`${u} `);
+alert(u);
+
+let boolean = 'true';
+console.log(boolean);
+document.write(`${boolean} `);
+alert(boolean);
+
+let bool= 'false';
+console.log(bool);
+document.write(bool);
+alert(bool);
