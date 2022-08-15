ecbd214707b2a8a4276b1e24e819e98e6beb8462
1oleg5 <panolegv@gmail.com>
2022-08-15 23:15:00 +0300

    index

--- JavaScript/Lesson2/main.js
+++ JavaScript/Lesson2/main.js
@@ -1,6 +1,11 @@
-let a = 100;
-let b = 'name';
-let c = 1;
-console.log(c);
-console.log(a);
-console.log(b);
+let a = [55,85,96,74,332,879,412,'okten',47,11];
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a[6]);
+console.log(a[7]);
+console.log(a[8]);
+console.log(a[9]);
