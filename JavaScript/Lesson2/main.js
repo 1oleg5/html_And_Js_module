fc6d2a4d4bca03ff3479a875ee998366f79e8052
1oleg5 <panolegv@gmail.com>
2022-08-17 01:17:01 +0300

    index2

--- JavaScript/Lesson2/main.js
+++ JavaScript/Lesson2/main.js
@@ -12,12 +12,12 @@ console.log(a[9]);
 
 
 
+
 let book1 = {
     title: 'Simpsons',
     pageCount: '249',
     genre: 'Comedy'
 }
-console.log(book1);
 
 
 let book2 = {
@@ -25,7 +25,6 @@ let book2 = {
     pageCount: '127',
     genre: 'Study'
 }
-console.log(book2);
 
 
 let book3 = {
@@ -33,4 +32,59 @@ let book3 = {
     pageCount: '278',
     genre: 'Study'
 }
-console.log(book3);
+console.log(book1, book2, book3,);
+
+
+
+
+let books1 = {
+    title: 'Ghost',
+    pageCount: '847',
+    genre: 'Fantasy',
+    authors: [name = 'Judy', age = 47]
+}
+
+
+let books2 = {
+    title: 'How to build a house',
+    pageCount: '952',
+    genre: 'Engineering',
+    authors: [name = 'Antony S', age = '28']
+}
+
+
+let books3 = {
+    title: 'Love story',
+    pageCount: '246',
+    genre: 'Love',
+    authors: [name = 'Jessika K', age = '56']
+}
+console.log(books1, books2, books3);
+
+
+
+
+
+
+let n10 = [
+    {name: 'Vasyl', username: 'Vasilius', password: 1234},
+    {name: 'Jessika', username: 'Jess', password: 4321},
+    {name: 'John', username: 'Jjj', password: 2314},
+    {name: 'Sergey', username: 'OnlyS', password: '1qa3'},
+    {name: 'Ivan', username: 'Ivan1987', password: 8520},
+    {name: 'Artem', username: 'Artemon25', password: 44601},
+    {name: 'Tamila', username: 'Ta_mila', password: 74213},
+    {name: 'Viktor', username: 'V12', password: 371327},
+    {name: 'Gennadiy', username: 'Tachka17', password: 13752},
+    {name: 'Volodymyr', username: 'Vovan55', password: 1723}
+]
+console.log(n10[0].password);
+console.log(n10[1].password);
+console.log(n10[2].password);
+console.log(n10[3].password);
+console.log(n10[4].password);
+console.log(n10[5].password);
+console.log(n10[6].password);
+console.log(n10[7].password);
+console.log(n10[8].password);
+console.log(n10[9].password);
