10bdb3e8c4d9c9f31c4f691cefb5a1c8e76f0a91
1oleg5 <panolegv@gmail.com>
2022-08-17 00:18:20 +0300

    index

--- JavaScript/Lesson2/main.js
+++ JavaScript/Lesson2/main.js
@@ -9,3 +9,28 @@ console.log(a[6]);
 console.log(a[7]);
 console.log(a[8]);
 console.log(a[9]);
+
+
+
+let book1 = {
+    title: 'Simpsons',
+    pageCount: '249',
+    genre: 'Comedy'
+}
+console.log(book1);
+
+
+let book2 = {
+    title: 'English',
+    pageCount: '127',
+    genre: 'Study'
+}
+console.log(book2);
+
+
+let book3 = {
+    title: 'Geography',
+    pageCount: '278',
+    genre: 'Study'
+}
+console.log(book3);
